77225255096619e0bb90741bdf86da1b3493d7d2
Yevhen3 <114805048+Yevhen3@users.noreply.github.com>
2022-10-29 13:55:42 +0300

    Update abstract_class.cs

--- abstract_class.cs
+++ abstract_class.cs
@@ -1,44 +1,43 @@
 abstract class GeoObject
 {
-    protected double coordX; 
+    protected double coordX;
     protected double coordY;
     protected string name;
     protected string description;
-    
-    public  double CoordX
-    {
-        set { coordX = value; }
-    }
-    public  double CoordY { set { coordY = value; } }
-    public  string Name {set { name = value; } }
-    public  string Description { set {description = value; }}
 
-    public virtual void Info(){}
-    
-    
+    public double CoordX{ get { return coordX; } set { coordX = value;}}
+    public double CoordY { get { return coordY; } set { coordY = value; } }
+    public string Name { get { return name; } set { name = value; } }
+    public string Description { get { return description; } set { description = value; } }
+
+    public virtual void Info() { Console.Write($"Name of object: {Name}, coordinats: ({CoordX};{CoordY}),"); }
+
+
 }
-class  River:GeoObject
-{    
-     double speed;
-     double length;
-    public int Speed {set { speed = value; } }
-    public int Length { set { length = value; } }
+class River : GeoObject
+{
+    double speed;
+    double length;
+    public double Speed { get { return speed; } set { speed = value; } }
+    public double Length { get { return length; } set { length = value; } }
 
     public override void Info()
     {
-        Console.WriteLine($"Name of the river is {name}, coordinats of river are ({coordX};{coordY}), speed is {speed}м/с, length is {length}км description: {description},");
+        base.Info();
+        Console.WriteLine($" speed: {Speed}м/с, length: {Length}км, description: {Description}");
     }
-    
+
 }
 
 class Mountain : GeoObject
 {
-    protected double highestPoint;
-    public double HighestPoint { set { highestPoint = value; } }
+    double highestPoint;
+    public double HighestPoint { get { return highestPoint; } set { highestPoint = value; } }
 
     public override void Info()
     {
-      Console.WriteLine(  $"Name of the mountain is {name}, coordinats of mountain are: ({coordX};{coordY}), highest point: {highestPoint}, description: {description},");
+        base.Info();
+        Console.WriteLine($" highest point: {HighestPoint}m, description: {Description}");
     }
 
 
